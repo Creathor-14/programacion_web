45253b30b60945d0b1612abe97338afe201a36fd
Mredo13 <114360114+Mredo13@users.noreply.github.com>
2023-06-24 17:26:33 -0400

    Carpeta

---
+++ restautant/api/views.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+from .serializer import PlatoSerializer
+from crud.models import Plato
+
+# Create your views here.
+
+class PlatoViewSet(viewsets.ModelViewSet):
+    queryset = Plato.objects.all()
+    serializer_class = PlatoSerializer
---
+++ restautant/core/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import * 
+
+
+urlpatterns = [
+    path('', root),
+    path('home/', home, name='home'),
+    path('contacto/', contacto, name='contacto'),
+    path('nosotros/', nosotros, name='nosotros'),
+    path('historia/', historia, name='historia'),
+    path('chistes/', chistes, name='chistes'),
+    path('productos/', productos, name='productos'),
+]
