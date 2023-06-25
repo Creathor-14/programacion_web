da1cc0f6e243acf9fbbada5689d074eb1ecf61fe
ElAdminExe <thomas.rene.q.e@gmail.com>
2023-06-24 22:16:43 -0400

    borrates makinals

--- restautant/api/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from crud.models import Plato
-
-class PlatoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Plato
-        # fields = ("categoria","nombre")
-        fields = '__all__'
--- restautant/api/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-from .serializer import PlatoSerializer
-from crud.models import Plato
-
-# Create your views here.
-
-class PlatoViewSet(viewsets.ModelViewSet):
-    queryset = Plato.objects.all()
-    serializer_class = PlatoSerializer
