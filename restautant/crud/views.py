5bf94bbafcadbaea9d5997561842c05ae5cd8068
ElAdminExe <thomas.rene.q.e@gmail.com>
2023-06-25 20:15:38 -0400

    new plato crud

--- restautant/crud/views.py
+++ restautant/crud/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, HttpResponse, redirect, reverse
 from .models import *
 from .forms import *
 
@@ -9,3 +9,42 @@ def root(request):
 def plato_list(request):
     context = {'platos': Plato.objects.all()}
     return render(request,'crud/platos.html',context)
+
+def plato_new(request):
+    if request.method == 'POST':
+        form = PlatoForm(request.POST, request.FILES)
+        if form.is_valid():
+            nombre = form.cleaned_data.get('nombre')
+            categoria = form.cleaned_data.get('categoria')
+            precio = form.cleaned_data.get('precio')
+
+            obj = Plato.objects.create(
+                nombre = nombre,
+                categoria = categoria,
+                precio = precio,
+            )
+            obj.save()
+            return redirect(reverse('platos')+ '?OK')
+        else:
+            return redirect(reverse('platos')+ '?FAIL')
+    else:
+        form = PlatoForm
+    return render(request,'crud/plato-new.html',{'form':form})
+
+def plato_update(request,plato_id):
+    try:
+        plato = Plato.objects.get(id = plato_id)
+        form = PlatoForm(instance=plato)
+
+        if request.method == 'POST':
+            form = PlatoForm(request.POST, request.FILES, instance=videojuego)
+            if form.is_valid():
+                form.save()
+                return redirect(reverse('videogames') + '?UPDATED')
+            else:
+                return redirect(reverse('videogame-edit') + videogame_id) 
+
+        context = {'form':form}
+        return render(request,'crud/videogame-edit.html',context)
+    except:
+        return redirect(reverse('videogames') + '?NO_EXISTS')
