76c739d19cb7deac3628fe30d2a31ba167b9a891
ElAdminExe <thomas.rene.q.e@gmail.com>
2023-08-29 20:08:51 -0400

    Usuarios QA

--- restautant/login_app/views.py
+++ restautant/login_app/views.py
@@ -20,35 +20,28 @@ def register(request):
         return render(request,'login_app/register.html')
     else:
         if request.method == 'POST':
-            print("llege")
             errors = Usuario.objects.validador_campos(request.POST)
-
             if len(errors) > 0:
                 for key, value in errors.items():
                     messages.error(request,value)
-
                 #Si se produce un error pero no queremos perder los datos....
                 request.session['registro_nombre'] = request.POST['nombre']
                 request.session['registro_apellido'] = request.POST['apellido']
                 request.session['registro_email'] = request.POST['email']
                 request.session['level_mensaje'] = 'alert-danger'
-
+                messages.success(request, "error!!!!")
             else:
                 request.session['registro_nombre'] = ""
                 request.session['registro_apellido'] = ""
                 request.session['registro_email'] = ""
-
                 nombre = request.POST['nombre']
                 apellido = request.POST['apellido']
                 email = request.POST['email']
                 clave_hash = bcrypt.hashpw(request.POST['clave'].encode(), bcrypt.gensalt()).decode()
-
                 obj = Usuario.objects.create(nombre=nombre, apellido=apellido,email=email,clave=clave_hash)
                 messages.success(request, "Usuario registrado con éxito!!!!")
                 request.session['level_mensaje'] = 'alert-success'
-            
             return render(request,'login_app/login.html')
-
         return render(request, 'login_app/login.html')
     
 def login(request):
